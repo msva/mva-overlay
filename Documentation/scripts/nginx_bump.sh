450db7fe10ad195d9f3d4f1f5a271ecfb9266eca
Vadim A. Misbakh-Soloviov <git@mva.name>
2015-07-04 18:13:04 +0600

    www-servers/nginx: bump modules

---
+++ Documentation/scripts/nginx_bump.sh
@@ -0,0 +1,8 @@
+cd www-servers/nginx
+awk \
+	'/^# .*https:\/\/github.com/{print gensub(/.*(https:\/\/.*\/tags).*/,"\\1","",$0)}' \
+	nginx-1.9.2.ebuild | while read taglink; do
+curl -s ${taglink} | awk \
+	'/href=.*archive.*.tar.gz/{print "https://github.com" gensub(/.*href="(.*\/archive\/.*.tar.gz)".*/,"\\1","",$0); exit}';
+done
+
