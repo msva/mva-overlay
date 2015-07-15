c0a60f30b0a26b6288508de2f6060406adfdcb52
Vadim A. Misbakh-Soloviov <git@mva.name>
2015-07-15 19:47:48 +0600

    nginx: bump
    Signed-off-by: Vadim A. Misbakh-Soloviov <git@mva.name>

--- Documentation/scripts/nginx_bump.sh
+++ Documentation/scripts/nginx_bump.sh
@@ -1,7 +1,7 @@
-cd www-servers/nginx
+#cd www-servers/nginx
 awk \
 	'/^# .*https:\/\/github.com/{print gensub(/.*(https:\/\/.*\/tags).*/,"\\1","",$0)}' \
-	nginx-1.9.2.ebuild | while read taglink; do
+	nginx-1.9.3.ebuild | while read taglink; do
 curl -s ${taglink} | awk \
 	'/href=.*archive.*.tar.gz/{print "https://github.com" gensub(/.*href="(.*\/archive\/.*.tar.gz)".*/,"\\1","",$0); exit}';
 done
