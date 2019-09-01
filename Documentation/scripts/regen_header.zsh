8a22aa74b7f2262cc47e7455440316191ea890d9
Vadim A. Misbakh-Soloviov <git@mva.name>
2019-09-01 21:46:14 +0700

    meta: fix headers

--- Documentation/scripts/regen_header.zsh
+++ Documentation/scripts/regen_header.zsh
@@ -1,3 +1,3 @@
 #!zsh
 DIR=${1:-.}
-find "${DIR}" -name '*.ebuild' -print0 | xargs -0 sed -r -e "1i# Copyright 1999-$(date +%Y) Gentoo Foundation" -e "1i# Distributed under the terms of the GNU General Public License v2" -e "1,3{/^#/d}" -e "4,5{/^EAPI=/s|=[[:punct:]]*([[:digit:]]*)[[:punct:]]*|=\1|g}" -i
+find "${DIR}" -name '*.ebuild' -print0 | xargs -0 sed -r -e "1i# Copyright 1999-$(date +%Y) Gentoo Authors" -e "1i# Distributed under the terms of the GNU General Public License v2" -e "1,3{/^#/d}" -e "4,5{/^EAPI=/s|=[[:punct:]]*([[:digit:]]*)[[:punct:]]*|=\1|g}" -i
