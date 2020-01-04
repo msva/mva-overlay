996f5b48c1973612ef832d59375c1716cc1209e9
Vadim A. Misbakh-Soloviov <git@mva.name>
2020-01-04 15:17:18 +0700

    Revert "meta: metadata regeneration (now `jer` should be totally happy)"
    This reverts commit b1ecf5440be65be28978cfdc0bf07a3fbf381401.

--- Documentation/scripts/regen_all_metadata.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-REPO_ROOT="${$(git rev-parse --show-toplevel):-.}"
-
-find . -mindepth 3 -maxdepth 3 -type f -name 'metadata.xml' | \
-{ [[ $@ ]] && grep $@ || cat; } | \
-xargs -I{} lua ${REPO_ROOT}/Documentation/scripts/update_metadata_xml.lua {}
